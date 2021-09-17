0a02522aa1cffb914ce09baac0d291ac1f614edd
hien nguyen <thuhiennguyen198@gmail.com>
2021-09-17 14:15:23 -0400

    done with cat model

--- cats99/app/models/cat.rb
+++ cats99/app/models/cat.rb
@@ -1,4 +1,14 @@
-class Cat < ApplicationRecord
-    validates :birth_date,:color,:name,:sex,:description, presence: true
 
+require "action_view"
+class Cat < ApplicationRecord
+include ActionView::Helpers::DateHelper
+    COLORS = ["gray", "black", "white", "brown"]
+    validates :birth_date,:color,:name,:sex,:description, presence: true
+    validates :color, inclusion: COLORS
+    validates :color, inclusion: ["M", "F"]
+    def age
+        time_ago_in_words(:birth_date)
+    end
+
+    
 end
