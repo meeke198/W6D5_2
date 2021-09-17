dbe836a3c0a0117eaa7c3f1f7daa551866dac0fc
hien nguyen <thuhiennguyen198@gmail.com>
2021-09-17 15:48:32 -0400

    done with show and index html

--- cats99/app/controllers/cats_controller.rb
+++ cats99/app/controllers/cats_controller.rb
@@ -9,6 +9,20 @@ class CatsController < ApplicationController
         @cat = Cat.find(params[:id])
     end
 
+    def new
+        @cat = Cat.new(cat_params)
+        render :new 
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+    end
+
     private
     def cat_params
         params.require(:cat).permit(:birth_date,:color,:name,:sex,:description)
